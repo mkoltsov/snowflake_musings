1f915be83b35cd7d5e69dad5db321a234d8dfdb1
mkoltsov <intimatik@gmail.com>
2026-02-13 00:17:22 -0500

    missing files added

--- api.py
+++ api.py
@@ -2,16 +2,19 @@
 from snowflake.snowpark import Session
 # create a dictionary with the connection parameters
 connection_parameters_dict = {
-
+    "account": "MTLPIOD-XTC20354", # replace with your Snowflake account
+    "user": "Intimatik", # replace with your username
+    "password": "aZ5^YP$kuKtP$bB3", # replace with your password
     "role": "SYSADMIN",
     "warehouse": "BAKERY_WH",
     "database": "BAKERY_DB",
-    "schema": "SNOWPARK"
+    "schema": "ORDERS"
 }
 # create the session object
 my_session = Session.builder.configs(connection_parameters_dict).create()
 # select the current timestamp
-ts = my_session.sql("select current_timestamp()").collect()
+# ts = my_session.sql("select current_timestamp()").collect()
+ts = my_session.sql("select delivery_date, baked_good_type, sum(quantity) as total_quantity from CUSTOMER_ORDERS group by all;").collect()
 # print the output to the console
 print(ts)
 # close the session
